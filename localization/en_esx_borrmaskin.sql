591da43013fc666294e70cb3cc542df85b09c932
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-23 03:52:06 +0000

    fixed sql import english locale

---
+++ localization/en_esx_borrmaskin.sql
@@ -0,0 +1,11 @@
+USE `essentialmode`;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+    ('drill', 'Drill', 1)
+;
+
+INSERT INTO `shops` (name, item, price) VALUES
+	('TwentyFourSeven','drill',1000),
+	('RobsLiquor','drill',1000),
+	('LTDgasoline','drill',1000)
+;
